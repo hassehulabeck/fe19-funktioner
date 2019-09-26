59590d0d4a67a28a07943b001dc63fffab4af986
hassehulabeck <hansbudandersson@gmail.com>
2019-09-26 12:57:17 +0200

    Added roman numerals

--- funktioner-4.js
+++ funktioner-4.js
@@ -5,7 +5,8 @@ var romanNumerals = [
     "I", "II", "III", "IV", "V", "VI", "VII", "IX", "X"
 ];
 
-function kingMaker(antalNamn) {
+// Huruvida vi ska ha romerska siffror eller inte bestäms av värdet på argumentet numerals. Där har jag valt att sätta ett default-värde på det, vilket gör att om vi inte skickar med det i funktionsanropet, så sätts det till false och koden i funktionen körs utan problem.
+function kingMaker(antalNamn, numerals = false) {
     var kingString = "";
 
     for (let i = 0; i < antalNamn; i++) {
@@ -16,8 +17,14 @@ function kingMaker(antalNamn) {
         kingString += kingNames[slump];
     }
 
+    if (numerals) {
+        let slump = Math.floor(Math.random() * romanNumerals.length);
+        kingString += " " + romanNumerals[slump];
+    }
+
     return kingString;
 }
 
-var theNextKing = kingMaker(1);
+// I anropet kan vi skriva argumentet som nedan, eller enbart true om vi hellre vill det.
+var theNextKing = kingMaker(1, numerals = true);
 var anotherKing = kingMaker(2);
